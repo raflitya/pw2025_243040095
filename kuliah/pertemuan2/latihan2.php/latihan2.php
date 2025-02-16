a938498861d2a3f1b7a28f3d797b5a84956c9c26
raflitya <raflitya123@gmail.com>
2025-02-16 20:01:27 +0700

    menyelesaikan tugas 1

---
+++ kuliah/pertemuan2/latihan2.php/latihan2.php
@@ -0,0 +1,10 @@
+<?php
+
+$nama = "Raflitya Rahman";
+$email = "raflitya123@gmail.com";
+$pekerjaan = "mahasiswa";
+$jurusan = "teknik informatika";
+$universitas = "UNPAS";
+echo "<h1>$nama</h1>";
+echo "<p>$email</p>";
+echo "Hallo, nama saya $nama, saya adalah seorang $pekerjaan unpas di $jurusan $universitas";
